d742c9a681eeea8803092d5ec7dc26d1e88e9402
Tai Truong V <tai.truong@asiantech.vn>
2019-08-15 15:43:45 +0700

    add pnumber

---
+++ tai.truong/PNUMBER.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int a) {
+    if (a < 2) return false;
+    if (a == 2) return true;
+    int count = 0;
+    for (int i = 3; i <= a; i += 1) if (a % i == 0) count++;
+    if (count ==2){
+        return true;
+    } else return false;
+
+}
+
+int main() {
+    int a, b; cin >> a >> b;
+    for (; a <= b; a++) if (isPrime(a)) cout << a << '\n';
+}
